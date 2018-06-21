04c8aa5b944eaa27c84180f59b06210cfc414f7b
Vanessa Toro <vanetoro84@gmail.com>
2018-06-21 17:48:14 +0000

    Fixed broken edit route and polished edit page

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -23,28 +23,16 @@ class EventController < ApplicationController
  end
 
 
-
- get '/events/:slug/edit' do
-    @event = Event.find_by_slug(params[:slug])
-   if Helpers.logged_in?(session) && Helpers.current_user(session).id == @event.host_id
-     @venues = Venue.all
-     erb :'/events/edit_event'
-   else
-        redirect '/events'
-    end
- end
-
-
  post '/events/create_event' do
    if params[:name].empty? || params[:date].empty?
      flash[:message] = 'Please make sure to enter Event name and date'
      redirect '/new'
-  else
-   @event = Event.create(name: params[:name], date: params[:date])
-   @host = Host.find(session[:host_id])
- end
+   else
+     @event = Event.create(name: params[:name], date: params[:date])
+     @host = Host.find(session[:host_id])
+  end
    if params[:new_venue].empty? && params[:venue] == nil
-    flash[:message] = 'Please make sure to choose a venue or enter a new venue'
+     flash[:message] = 'Please make sure to choose a venue or enter a new venue'
      redirect '/new'
    elsif !params[:new_venue].empty?
      @venue = Venue.create(name: params[:new_venue],location: params[:location])
@@ -57,12 +45,28 @@ class EventController < ApplicationController
      redirect "/events"
  end
 
+
+ get '/events/:slug/edit' do
+    @event = Event.find_by_slug(params[:slug])
+   if Helpers.logged_in?(session) && Helpers.current_user(session).id == @event.host_id
+     @venues = Venue.all
+     erb :'/events/edit_event'
+   else
+    redirect '/events'
+   end
+ end
+
  patch '/events/:slug' do
    @event = Event.find_by_slug(params[:slug])
    @host = Host.find(session[:host_id])
    if Helpers.logged_in?(session) && Helpers.current_user(session).id == @event.host_id
      @event.name = params[:name]
      @event.date = params[:date]
+      if  !params[:new_venue].empty?
+        @event.venue = Venue.create(name: params[:new_venue],location: params[:location])
+      else
+        @event.venue = Venue.find(params[:venue])
+      end
      @event.save
      flash[:message] = 'Successfully updated event.'
      redirect "/events"
@@ -77,11 +81,11 @@ class EventController < ApplicationController
    @host = @event.host
    if @host.id == Helpers.current_user(session).id
      @event.destroy
-     redirect "/events/show_all"
+     flash[:message] = 'You successfully deleted this event.'
+     redirect "/events/"
    else
      redirect '/events'
    end
  end
-
 
 end
